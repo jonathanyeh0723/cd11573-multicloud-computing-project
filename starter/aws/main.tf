4cf54d3d76ab95731dc047fd9da0f5a8aa60769c
jonathanyeh0723 <johnnyyeh0723@gmail.com>
2024-05-13 17:13:01 +0800

    modified aws dynamoDB instance name

--- starter/aws/main.tf
+++ starter/aws/main.tf
@@ -214,7 +214,7 @@ resource "aws_s3_bucket" "example" {
 }
 
 resource "aws_dynamodb_table" "udacity-jonathan-aws-dynamodb" {
-  name           = "GameScores"
+  name           = "udacity-jonathan-aws-dynamodb"
   billing_mode   = "PROVISIONED"
   read_capacity  = 20
   write_capacity = 20
