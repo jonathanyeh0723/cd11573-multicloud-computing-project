02b403ee966d9adec3a5b984a5f1d41f839e58d5
jonathanyeh0723 <johnnyyeh0723@gmail.com>
2024-05-13 16:34:55 +0800

    Address for aws - s3 + dynamodb

--- starter/aws/main.tf
+++ starter/aws/main.tf
@@ -204,3 +204,55 @@ variable "app_count" {
 
 ####### Your Additions Will Start Here ######
 
+resource "aws_s3_bucket" "example" {
+  bucket = "udacity-jonathan-aws-s3-bucket"
+
+  tags = {
+    Name        = "udacity-jonathan-aws-s3-bucket"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_dynamodb_table" "udacity-jonathan-aws-dynamodb" {
+  name           = "GameScores"
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 20
+  write_capacity = 20
+  hash_key       = "UserId"
+  range_key      = "GameTitle"
+
+  attribute {
+    name = "UserId"
+    type = "S"
+  }
+
+  attribute {
+    name = "GameTitle"
+    type = "S"
+  }
+
+  attribute {
+    name = "TopScore"
+    type = "N"
+  }
+
+  ttl {
+    attribute_name = "TimeToExist"
+    enabled        = false
+  }
+
+  global_secondary_index {
+    name               = "GameTitleIndex"
+    hash_key           = "GameTitle"
+    range_key          = "TopScore"
+    write_capacity     = 10
+    read_capacity      = 10
+    projection_type    = "INCLUDE"
+    non_key_attributes = ["UserId"]
+  }
+
+  tags = {
+    Name        = "udacity-jonathan-aws-dynamodb"
+    Environment = "Dev"
+  }
+}
